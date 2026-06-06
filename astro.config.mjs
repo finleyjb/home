9674b8d6044dcee4d9b15bb1233e55d85ca2ad07
Finley Baker <finleyb.dev@proton.me>
2026-06-06 16:07:46 -0600

    Hopefully turn sessions off for reals this time

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  session: {},
+  output: 'static',
+  session: {
+    driver: {
+      entrypoint: 'unstorage/drivers/null',
+    },
+  },
 });
 
