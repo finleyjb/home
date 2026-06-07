25f1f98a93de7bf161a7277a0bb790c383748948
Finley Baker <finleyb.dev@proton.me>
2026-06-07 17:45:33 -0600

    Use local fonts instead of relying on CDN

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,47 @@
 // @ts-check
 
-import cloudflare from '@astrojs/cloudflare';
-import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  output: 'static',
+  output: "static",
   session: {
     driver: {
-      entrypoint: 'unstorage/drivers/null',
+      entrypoint: "unstorage/drivers/null",
     },
   },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Zodiak",
+      cssVariable: "--font-zodiak",
+      fallbacks: ["serif"],
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/Zodiak-Variable.woff2"],
+            weight: "100 900",
+            style: "normal",
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "Plus Jakarta Sans",
+      cssVariable: "--font-plus-jakarta-sans",
+      fallbacks: ["sans-serif", "system-ui"],
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/PlusJakartaSans-Variable.woff2"],
+            weight: "200 800",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
 });
-
