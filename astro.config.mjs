13ee71091727e4596e868c8927a361f4c5412151
Finley Baker <finleyb.dev@proton.me>
2026-06-09 14:29:35 -0600

    Remove unused bespoke fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,49 +18,28 @@ export default defineConfig({
 
   fonts: [
     {
-      provider: fontProviders.local(),
-      name: 'Bespoke Slab',
-      cssVariable: '--font-bespoke-slab',
-      fallbacks: ['serif'],
-      options: {
-        variants: [
-          {
-            src: ['./src/assets/fonts/BespokeSlab-Variable.woff2'],
-            weight: '300 800',
-            style: 'normal',
-          },
-          {
-            src: ['./src/assets/fonts/BespokeSlab-VariableItalic.woff2'],
-            weight: '300 800',
-            style: 'italic',
-          },
-        ],
-      },
-    },
-
-    {
       provider: fontProviders.npm(),
-      name: 'Metropolis Display',
+      name: 'Metropolis',
       cssVariable: '--font-metropolis-display',
       fallbacks: ['sans-serif'],
       weights: ['700'],
       styles: ['normal'],
       options: {
         package: '@fontsource/metropolis',
-        file: '700.css',
+        file: 'latin-700.css',
       },
     },
 
     {
       provider: fontProviders.npm(),
-      name: 'Metropolis Body',
+      name: 'Metropolis',
       cssVariable: '--font-metropolis-body',
       fallbacks: ['sans-serif'],
       weights: ['400'],
       styles: ['normal'],
       options: {
         package: '@fontsource/metropolis',
-        file: '400.css',
+        file: 'latin-400.css',
       },
     },
   ],
